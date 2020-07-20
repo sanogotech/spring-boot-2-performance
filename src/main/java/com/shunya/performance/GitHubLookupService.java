f6a59ec140d1b59b6e1368bb71597d364a84cbef
SANOGOTECH <sanogotech@yahoo.fr>
2020-07-20 12:07:08 +0000

    Add files via upload

--- src/main/java/com/shunya/performance/GitHubLookupService.java
+++ src/main/java/com/shunya/performance/GitHubLookupService.java
@@ -16,21 +16,42 @@ public class GitHubLookupService {
 	private static final Logger logger = LoggerFactory.getLogger(GitHubLookupService.class);
 
 
-	@Autowired
-	private RestTemplate restTemplate;
+	//@Autowired
+	//private RestTemplate restTemplate;
 
 	/*
 	@Autowired
 	public GitHubLookupService(RestTemplateBuilder restTemplateBuilder) {
 		this.restTemplate = restTemplateBuilder.build();
 	}*/
+	
+	@Async
+	public CompletableFuture<User> findLightUser(String user) throws InterruptedException {
+		logger.info("Start  Async Task ..");
+		//logger.info("Looking up " + user);
+		String url = String.format("https://api.github.com/users/%s", user);
+		//User results = restTemplate.getForObject(url, User.class);
+		User  myuser = new  User();
+		myuser.setBlog("www.yahoo.fr");
+		myuser.setName("MyYahoo");
+		// Artificial delay of 1s for demonstration purposes
+		Thread.sleep(50000L);
+		logger.info("End Async Task");
+		logger.info("End Update Database ...");
+		return CompletableFuture.completedFuture(myuser);
+	}
 
 	@Async
 	public CompletableFuture<User> findUser(String user) throws InterruptedException {
 		logger.info("Start  Async Task ..");
 		//logger.info("Looking up " + user);
 		String url = String.format("https://api.github.com/users/%s", user);
-		User results = restTemplate.getForObject(url, User.class);
+		User results = null;
+		//User results = restTemplate.getForObject(url, User.class);
+		//TODO
+		//https://www.baeldung.com/spring-webclient-resttemplate
+		//WebClient Non-Blocking Client
+		//TODO
 		// Artificial delay of 1s for demonstration purposes
 		Thread.sleep(50000L);
 		logger.info("End Async Task");
